910ee3bfa354c78cdc6eaec7258ca65c7f38a099
Rajakumaran Arivumani <dExplorer@users.noreply.github.com>
2024-12-26 01:20:20 +0000

    func updates #2

--- dq_app.py
+++ dq_app.py
@@ -10,6 +10,15 @@ def get_expectation_by_id(exp_id, dq_expectations):
             return dq_expectation
     return None
 
+def get_dq_rules_by_dataset_id(dataset_id, dq_rules):
+    dq_rules_for_dataset = []
+
+    for dq_rule in dq_rules:
+        if dataset_id == dq_rule.dataset_id:
+            dq_rules_for_dataset.append(dq_rule)
+
+    return dq_rules_for_dataset
+
 
 def apply_dq_rules(batch, dq_rules, dq_expectations) -> list:
     dq_check_results = []
@@ -17,24 +26,36 @@ def apply_dq_rules(batch, dq_rules, dq_expectations) -> list:
     for dq_rule in dq_rules:
         dq_expectation = get_expectation_by_id(dq_rule.exp_id, dq_expectations)
 
-        if dq_expectation.exp_name == "ExpectColumnValuesToBeUnique":
-            # Define the Expectation to test:
-            expectation = gx.expectations.ExpectColumnValuesToBeUnique(
-                column=dq_rule.column_name
-            )
+        # Assign the GE function name to a string
+        gen_func_str = f"gen_func = gx.expectations.{dq_expectation.exp_name}"
+        # Get local variables
+        _locals = locals()
+        # Execute the function name assignment, this mutates the local namespace
+        exec(gen_func_str, globals(), _locals)
+        # Grab the newly defined function name from the local namespace dictionary and assign it to generic function variable
+        gen_func = _locals['gen_func']
+        # Pass function specific keyword arguments to the generic function
+        expectation = gen_func(**dq_rule.kwargs)
 
-        elif dq_expectation.exp_name == "ExpectColumnValuesToBeInSet":
-            expectation = gx.expectations.ExpectColumnValuesToBeInSet(
-                column=dq_rule.column_name, value_set=["equity", "mutual fund"]
-            )
+        # if dq_expectation.exp_name == "ExpectColumnValuesToBeUnique":
+        #     # Define the Expectation to test:
+        #     expectation = gx.expectations.ExpectColumnValuesToBeUnique(
+        #         # column=dq_rule.kwargs['column']
+        #         **dq_rule.kwargs
+        #     )
 
-        elif dq_expectation.exp_name == "ExpectColumnValuesToBeBetween":
-            expectation = gx.expectations.ExpectColumnValuesToBeBetween(
-                column=dq_rule.column_name, min_value=5, max_value=50
-            )
+        # elif dq_expectation.exp_name == "ExpectColumnValuesToBeInSet":
+        #     expectation = gx.expectations.ExpectColumnValuesToBeInSet(
+        #         **dq_rule.kwargs
+        #     )
 
-        else:
-            expectation = None
+        # elif dq_expectation.exp_name == "ExpectColumnValuesToBeBetween":
+        #     expectation = gx.expectations.ExpectColumnValuesToBeBetween(
+        #         **dq_rule.kwargs
+        #     )
+
+        # else:
+        #     expectation = None
 
         if expectation:
             # Test the Expectation:
@@ -85,23 +106,26 @@ def main():
         DQRule(
             rule_id="1",
             dataset_id="1",
-            column_name="asset_id",
             exp_id="1",
             rule_fail_action="abort",
+            column="asset_id",
         ),
         DQRule(
             rule_id="2",
             dataset_id="1",
-            column_name="asset_type",
             exp_id="2",
             rule_fail_action="abort",
+            column="asset_type",
+            value_set=['equity', 'mutual fund']
         ),
         DQRule(
             rule_id="3",
             dataset_id="1",
-            column_name="asset_id",
             exp_id="3",
             rule_fail_action="proceed",
+            column="asset_id",
+            min_value=5,
+            max_value=50
         ),
     ]
 
@@ -113,13 +137,16 @@ def main():
     )
     # dataset.add_dq_rule()
 
+    # Get DQ rules defined for the dataset
+    dq_rules_for_dataset = get_dq_rules_by_dataset_id(dataset.dataset_id, dq_rules)
+
     # Define the context
     context = gx.get_context()
 
     # Use the `pandas_default` Data Source to retrieve a Batch of sample Data from a data file:
     batch = context.data_sources.pandas_default.read_csv(dataset.file_path)
 
-    dq_check_results = apply_dq_rules(batch, dq_rules, dq_expectations)
+    dq_check_results = apply_dq_rules(batch, dq_rules_for_dataset, dq_expectations)
 
     if dq_check_results:
         for dq_check_result in dq_check_results:
--- dq_rule.py
+++ dq_rule.py
@@ -1,7 +1,8 @@
 class DQRule:
-    def __init__(self, rule_id, dataset_id, column_name, exp_id, rule_fail_action):
+    def __init__(self, rule_id, dataset_id, exp_id, rule_fail_action, **kwargs):
         self.rule_id = rule_id
         self.dataset_id = dataset_id
-        self.column_name = column_name
+        # self.column_name = column_name
         self.exp_id = exp_id
         self.rule_fail_action = rule_fail_action
+        self.kwargs = kwargs
